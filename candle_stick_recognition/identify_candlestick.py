a8d95fcc5b6acd797f0d9a57fb1d11e72fe2deb9
CanerIrfanoglu <canerirfanoglu@gmail.com>
2020-02-07 19:11:11 -0500

    Separated candle_rankings & added plotting

---
+++ candle_stick_recognition/identify_candlestick.py
@@ -0,0 +1,83 @@
+import numpy as np
+from candle_rankings import candle_rankings
+import talib
+from itertools import compress
+
+def recognize_candlestick(df):
+    """
+    Recognizes candlestick patterns and appends 2 additional columns to df;
+    1st - Best Performance candlestick pattern matched by www.thepatternsite.com
+    2nd - # of matched patterns
+    """
+
+    op = df['open'].astype(float)
+    hi = df['high'].astype(float)
+    lo = df['low'].astype(float)
+    cl = df['close'].astype(float)
+
+    candle_names = talib.get_function_groups()['Pattern Recognition']
+
+    # patterns not found in the patternsite.com
+    exclude_items = ('CDLCOUNTERATTACK',
+                     'CDLLONGLINE',
+                     'CDLSHORTLINE',
+                     'CDLSTALLEDPATTERN',
+                     'CDLKICKINGBYLENGTH')
+
+    candle_names = [candle for candle in candle_names if candle not in exclude_items]
+
+
+    # create columns for each candle
+    for candle in candle_names:
+        # below is same as;
+        # df["CDL3LINESTRIKE"] = talib.CDL3LINESTRIKE(op, hi, lo, cl)
+        df[candle] = getattr(talib, candle)(op, hi, lo, cl)
+
+
+    df['candlestick_pattern'] = np.nan
+    df['candlestick_match_count'] = np.nan
+    for index, row in df.iterrows():
+
+        # no pattern found
+        if len(row[candle_names]) - sum(row[candle_names] == 0) == 0:
+            df.loc[index,'candlestick_pattern'] = "NO_PATTERN"
+            df.loc[index, 'candlestick_match_count'] = 0
+        # single pattern found
+        elif len(row[candle_names]) - sum(row[candle_names] == 0) == 1:
+            # bull pattern 100 or 200
+            if any(row[candle_names].values > 0):
+                pattern = list(compress(row[candle_names].keys(), row[candle_names].values != 0))[0] + '_Bull'
+                df.loc[index, 'candlestick_pattern'] = pattern
+                df.loc[index, 'candlestick_match_count'] = 1
+            # bear pattern -100 or -200
+            else:
+                pattern = list(compress(row[candle_names].keys(), row[candle_names].values != 0))[0] + '_Bear'
+                df.loc[index, 'candlestick_pattern'] = pattern
+                df.loc[index, 'candlestick_match_count'] = 1
+        # multiple patterns matched -- select best performance
+        else:
+            # filter out pattern names from bool list of values
+            patterns = list(compress(row[candle_names].keys(), row[candle_names].values != 0))
+            container = []
+            for pattern in patterns:
+                if row[pattern] > 0:
+                    container.append(pattern + '_Bull')
+                else:
+                    container.append(pattern + '_Bear')
+            rank_list = [candle_rankings[p] for p in container]
+            if len(rank_list) == len(container):
+                rank_index_best = rank_list.index(min(rank_list))
+                df.loc[index, 'candlestick_pattern'] = container[rank_index_best]
+                df.loc[index, 'candlestick_match_count'] = len(container)
+    # clean up candle columns
+    df.drop(candle_names, axis = 1, inplace = True)
+
+    return df
+
+
+
+
+
+
+
+
---
+++ candle_stick_recognition/plotting_candles_example.py
@@ -0,0 +1,20 @@
+from plotly.offline import plot
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('./btc_data.csv')
+
+o = df['open'].astype(float)
+h = df['high'].astype(float)
+l = df['low'].astype(float)
+c = df['close'].astype(float)
+
+
+trace = go.Candlestick( #x= pd.to_datetime(dfohlc.index.values),
+            open=o,
+            high=h,
+            low=l,
+            close=c)
+data = [trace]
+
+plot(data, filename='go_candle1.html')
